4d744a260a7b5a257948655c55216998618d521b
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-03-13 13:38:27 -0400

    Initial implementation of QGraphicsArcItem

--- qgraphics_arc_item.cpp
+++ qgraphics_arc_item.cpp
@@ -1,7 +1,12 @@
 #include "qgraphics_arc_item.hpp"
-
 #include <QPainter>
 #include <QStyleOptionGraphicsItem>
 #include <QWidget>
+
+void QGraphicsArcItem::paint(QPainter * painter,
+                             const QStyleOptionGraphicsItem * option,
+                             QWidget * widget) {
+    painter->drawArc(rect(), startAngle(), spanAngle());
+}
 
 
