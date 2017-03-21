311160df1a32ffa3f275a39be4c34ed87cb1b51f
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-03-21 10:14:48 -0400

    Initial implementation of repl_widget

--- repl_widget.cpp
+++ repl_widget.cpp
@@ -1,1 +1,22 @@
+#include <QApplication>
 #include "repl_widget.hpp"
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QLabel>
+
+REPLWidget::REPLWidget(QWidget * parent) : QWidget(parent) {
+    text = new QLineEdit();
+    label = new QLabel("vtscript>");
+
+    QHBoxLayout * layout = new QHBoxLayout();
+    layout->addWidget(label);
+    layout->addWidget(text);
+    setLayout(layout);
+
+    QEventLoop::connect(text, SIGNAL(returnPressed()), this, SLOT(runLine()));
+    QEventLoop::connect(text, SIGNAL(returnPressed()), text, SLOT(clear()));
+}
+
+void REPLWidget::runLine() {
+    emit lineEntered(text->text());
+}
--- repl_widget.hpp
+++ repl_widget.hpp
@@ -2,11 +2,21 @@
 #define REPL_WIDGET_HPP
 
 #include <QWidget>
+class QLineEdit;
+class QLabel;
 
 class REPLWidget : public QWidget {
+    Q_OBJECT
+private:
+    QLineEdit * text;
+    QLabel * label;
+public:
     // Default construct a REPLWidget
     REPLWidget(QWidget * parent = nullptr);
-    
+public slots:
+    // Run the currently entered line
+    void runLine();
+signals:
     // A signal that sends the current edited text as a QString when the
     // return key is pressed.
     void lineEntered(QString);
