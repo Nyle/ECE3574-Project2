f7aece8c83e558f811a9d37fed49d77f473d3798
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-03-21 10:13:58 -0400

    Initial implementation of canvas_widget

--- canvas_widget.cpp
+++ canvas_widget.cpp
@@ -1,4 +1,15 @@
 #include "canvas_widget.hpp"
 
 #include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
+CanvasWidget::CanvasWidget(QWidget * parent) : QWidget(parent) {
+    scene = new QGraphicsScene();
+    view = new QGraphicsView(scene);
+    view->show();
+}
+
+void CanvasWidget::addGraphic(QGraphicsItem * item) {
+    scene->addItem(item);
+}
--- canvas_widget.hpp
+++ canvas_widget.hpp
@@ -1,13 +1,20 @@
 #ifndef CANVAS_WIDGET_HPP
 #define CANVAS_WIDGET_HPP
 
-#include<QWidget>
+#include <QWidget>
 class QGraphicsItem;
+class QGraphicsScene;
+class QGraphicsView;
 
 class CanvasWidget : public QWidget {
+    Q_OBJECT
+private:
+    QGraphicsScene * scene;
+    QGraphicsView * view;
+public:
     // Default construct a CanvasWidget
     CanvasWidget(QWidget * parent = nullptr);
-    
+public slots:
     // A public slot that accepts a signal in the form of a QGraphicsItem
     // pointer containing an object derived from QGraphicsItem to draw
     void addGraphic(QGraphicsItem * item);
