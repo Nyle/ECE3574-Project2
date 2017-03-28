13c9b3fe9b45a05f290905191a87fdb21e6c33f9
Nyle Rodgers <nyle.rodgers@gmail.com>
2017-03-28 15:01:57 -0400

    Add history functionality

--- repl_widget.cpp
+++ repl_widget.cpp
@@ -3,6 +3,7 @@
 #include <QLineEdit>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QKeyEvent>
 
 REPLWidget::REPLWidget(QWidget * parent) : QWidget(parent) {
     text = new QLineEdit();
@@ -15,8 +16,28 @@ REPLWidget::REPLWidget(QWidget * parent) : QWidget(parent) {
 
     QEventLoop::connect(text, SIGNAL(returnPressed()), this, SLOT(runLine()));
     QEventLoop::connect(text, SIGNAL(returnPressed()), text, SLOT(clear()));
+
+    text->installEventFilter(this);
+}
+
+bool REPLWidget::eventFilter(QObject* obj, QEvent *event) {
+    if (obj == text && event->type() == QEvent::KeyPress) {
+        QKeyEvent * keyEvent = static_cast<QKeyEvent*>(event);
+        if (keyEvent->key() == Qt::Key_Up && history_position != 0) {
+            history_position -= 1;
+            text->setText(history[history_position]);
+        } else if (keyEvent->key() == Qt::Key_Down &&
+                   history_position != history.size()) {
+            history_position += 1;
+            text->setText(history_position != history.size() ?
+                          history[history_position] : "");
+        }
+    }
+    return QWidget::eventFilter(obj, event);
 }
 
 void REPLWidget::runLine() {
+    history.push_back(text->text());
+    history_position = history.size();
     emit lineEntered(text->text());
 }
--- repl_widget.hpp
+++ repl_widget.hpp
@@ -2,17 +2,24 @@
 #define REPL_WIDGET_HPP
 
 #include <QWidget>
+#include <vector>
 class QLineEdit;
 class QLabel;
+class QKeyEvent;
 
 class REPLWidget : public QWidget {
     Q_OBJECT
 private:
     QLineEdit * text;
     QLabel * label;
+    std::vector<QString> history;
+    size_t history_position;
 public:
-    // Default construct a REPLWidget
+    // Default construct a REPL Widget
     REPLWidget(QWidget * parent = nullptr);
+
+    // Catch key press event
+    bool eventFilter(QObject* obj, QEvent *event);
 public slots:
     // Run the currently entered line
     void runLine();
